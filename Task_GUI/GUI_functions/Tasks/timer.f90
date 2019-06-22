c8a23850e84bf2112bc09797f028d506c0e70bf5
Anton Albertovich <38799794+AntonAlbertovich@users.noreply.github.com>
2019-06-22 16:30:53 -0500

    Delete timer.f90

--- Task_GUI/GUI_functions/Tasks/timer.f90
+++
@@ -1,5 +0,0 @@
-program test_system_clock
-  integer :: count, count_rate, count_max
-  call system_clock(count, count_rate, count_max)
-  write(*,*) count, count_rate, count_max
-end program test_system_clock
